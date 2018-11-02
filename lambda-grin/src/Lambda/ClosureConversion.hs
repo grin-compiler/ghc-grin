80425c30cfab8598a11ebc47ef6c8b7bbbfd0a47
Csaba Hruska <csaba.hruska@gmail.com>
2018-11-02 16:12:19 +0100

    working lambda lifting ; intial version of closure conversion (not needed for STG)

---
+++ lambda-grin/src/Lambda/ClosureConversion.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE LambdaCase #-}
+module Lambda.ClosureConversion where
+
+import Control.Monad.State
+import Control.Monad.Writer
+import Data.Functor.Foldable as Foldable
+import qualified Data.Foldable
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Transformations.Util
+import Transformations.Names hiding (mkNameEnv)
+
+import Lambda.Syntax
+import Lambda.Util
+
+
+type ClosM = WriterT [Def] NameM
+
+without :: Eq a => [a] -> [a] -> [a]
+without = foldr (filter . (/=)) -- Like \\ but removes all occurrences
+
+applyTo :: Exp -> [Name] -> Exp
+applyTo e []          = e
+applyTo (Var n) args  = App n (map Var args)
+applyTo e args        = AppExp e (map Var args)
+
+freeVars :: Exp -> [Name] -- Grab all the unbound variables in an expression
+freeVars = cata folder where
+  folder = \case
+    VarF v      -> [v]
+    AppF n e    -> n : mconcat e
+    LamF vs e   -> e `without` vs
+    e           -> Data.Foldable.fold e
+
+closConv :: [Name] -> Exp -> Exp
+closConv globals = cata folder where
+  folder (LamF vs e) = let vars = freeVars e `without` (globals ++ vs)
+                       in Lam (vars ++ vs) e `applyTo` vars
+  folder e = embed e
+
+liftLam :: Exp -> ClosM Exp
+liftLam = cataM folder where
+  folder = \case
+    LamF vs e -> do
+      fresh <- lift $ deriveNewName "closure" -- TODO: better name
+      tell [Def fresh vs e]
+      pure $ Var fresh
+
+    -- smash
+    AppExpF (Var n) args
+      | all isAtom args       -> pure $ App n args
+    LetF l1 (Let l2 e)        -> pure $ Let (l1 ++ l2) e
+    LetSF l1 (LetS l2 e)      -> pure $ LetS (l1 ++ l2) e
+    e                         -> pure $ embed e
+
+smash :: Exp -> Exp
+smash = cata folder where
+  folder = \case
+    LamF vs (Lam vs' e) -> Lam (vs ++ vs') e
+    AppExpF (Var n) args
+      | all isAtom args -> App n args
+    e                   -> embed e
+
+eliminateLams :: [Name] -> Program -> Program
+eliminateLams globals prg@(Program defs) = Program $ defs' ++ newDefs where
+  (Program defs', newDefs) = evalState (runWriterT . liftLam{- . smash . closConv (defNames ++ globals)-} $ prg) (mkNameEnv prg)
+  defNames = [n | Def n _ _ <- defs]
---
+++ lambda-grin/src/Lambda/Util.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE LambdaCase #-}
+module Lambda.Util where
+
+import Data.Functor.Foldable
+import qualified Data.Set as Set
+import qualified Data.Foldable
+
+import Transformations.Names hiding (mkNameEnv)
+import Transformations.Util hiding (foldNameDefExpF)
+import Lambda.Syntax
+
+mkNameEnv :: Exp -> NameEnv
+mkNameEnv exp = NameEnv mempty (cata folder exp) where
+  folder e = foldNameDefExpF Set.singleton e `mappend` Data.Foldable.fold e
+
+foldLocalNameDefExp :: (Monoid m) => (Name -> m) -> Exp -> m
+foldLocalNameDefExp f = foldLocalNameDefExpF f . project
+
+foldLocalNameDefExpF :: (Monoid m) => (Name -> m) -> ExpF a -> m
+foldLocalNameDefExpF f = \case
+  DefF _ args _ -> mconcat $ map f args
+  e             -> foldNameDefExpF f e
+
+foldNameDefExpF :: (Monoid m) => (Name -> m) -> ExpF a -> m
+foldNameDefExpF f = \case
+  DefF n args _         -> mconcat $ map f $ n : args
+  LetF bs _             -> mconcat $ map (f . fst) bs
+  LetRecF bs _          -> mconcat $ map (f . fst) bs
+  LetSF bs _            -> mconcat $ map (f . fst) bs
+  AltF (NodePat _ bs) _ -> mconcat $ map f bs
+  LamF bs _             -> mconcat $ map f bs
+  _                     -> mempty
+
+mapLocalNameExp :: (Name -> Name) -> Exp -> Exp
+mapLocalNameExp f = \case
+  -- def
+  Def n args e          -> Def n (map f args) e -- NOTE: do not touch top level function name
+  LetRec bs e           -> LetRec [(f n, a) | (n,a) <- bs] e
+  Let bs e              -> LetS [(f n, a) | (n,a) <- bs] e
+  LetS bs e             -> LetS [(f n, a) | (n,a) <- bs] e
+  Alt (NodePat n bs) e  -> Alt (NodePat n (map f bs)) e
+  Lam bs e              -> Lam (map f bs) e
+  -- use
+  App n l               -> App (f n) l
+  Var n                 -> Var (f n)
+  -- other
+  exp                   -> exp
+
+isAtom :: Exp -> Bool
+isAtom = \case
+  Var{} -> True
+  Lit{} -> True
+  _     -> False
