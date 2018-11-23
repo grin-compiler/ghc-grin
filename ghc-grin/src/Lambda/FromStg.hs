835fdce7a943b91e8fe30b2d71ba74aab41f2f8a
Csaba Hruska <csaba.hruska@gmail.com>
2018-11-23 20:23:39 +0100

    smarter translation; avoids extra closure layer

--- ghc-grin/src/Lambda/FromStg.hs
+++ ghc-grin/src/Lambda/FromStg.hs
@@ -78,14 +78,14 @@ visitArg = \case
 visitExpr :: C.Expr -> CG Exp
 visitExpr = \case
   C.StgLit lit          -> pure . Lit $ convertLit lit
+  C.StgApp fun []       -> Var <$> genName fun
   C.StgApp fun args     -> App <$> genName fun <*> mapM visitArg args
   C.StgConApp con args  -> Con <$> genConName con <*> mapM visitArg args
   C.StgOpApp op args ty -> case op of
-    C.StgPrimOp prim  -> App ("_prim_ghc_" <> (packName $ BS8.unpack prim) <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args
-    C.StgPrimCallOp _ -> App ("_prim__stg_primCall_TODO_" <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args -- TODO
-    C.StgFCallOp f    -> App ("_prim__stg_foreign_call " <> (TS.pack . show . P.pretty $ f) <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args -- TODO
+    C.StgPrimOp prim  -> App ("_ghc_" <> (packName $ BS8.unpack prim) <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args
+    C.StgPrimCallOp _ -> App ("_ghc__stg_primCall_TODO_" <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args -- TODO
+    C.StgFCallOp f    -> App ("_ghc__stg_foreign_call " <> (TS.pack . show . P.pretty $ f) <> (TS.pack . show . P.braces $ P.pretty ty)) <$> mapM visitArg args -- TODO
 
-  C.StgLam bs expr      -> Lam <$> mapM genName bs <*> visitExpr expr
   C.StgCase expr result alts  -> do
     scrutName <- genName result
     scrutExp <- visitExpr expr
@@ -100,6 +100,9 @@ visitExpr = \case
   C.StgLetNoEscape (C.StgNonRec b r) e  -> Let <$> ((\name exp -> [(name, exp)]) <$> genName b <*> visitRhs r) <*> visitExpr e
   C.StgLetNoEscape (C.StgRec bs) e      -> LetRec <$> mapM (\(b,r) -> (,) <$> genName b <*> visitRhs r) bs <*> visitExpr e
 
+  -- NOTE: STG must not contain lambdas
+  C.StgLam bs expr -> Lam <$> mapM genName bs <*> visitExpr expr
+
   expr -> error . printf "unsupported expr %s" $ show expr
 
 
@@ -109,11 +112,16 @@ visitRhs = \case
   C.StgRhsClosure [] _ bs e -> Lam <$> mapM genName bs <*> visitExpr e
   C.StgRhsClosure vs _ bs e -> AppExp <$> (Lam <$> mapM genName (vs' ++ bs) <*> visitExpr e) <*> mapM (pure . Var <=< genName) vs' where vs' = Set.toList . Set.fromList $ vs
 
+visitTopRhs :: C.Binder -> C.Rhs -> CG Def
+visitTopRhs b = \case
+  C.StgRhsClosure [] _ bs e -> Def <$> genName b <*> mapM genName bs <*> visitExpr e
+  rhs                       -> Def <$> genName b <*> pure [] <*> visitRhs rhs
+
 visitTopBinder :: C.TopBinding -> CG [Def]
 visitTopBinder = \case
   C.StgTopStringLit b s             -> sequence [Def <$> genName b <*> pure [] <*> pure (Lit $ LString s)]
-  C.StgTopLifted (C.StgNonRec b r)  -> sequence [Def <$> genName b <*> pure [] <*> visitRhs r]
-  C.StgTopLifted (C.StgRec bs)      -> sequence [Def <$> genName b <*> pure [] <*> visitRhs r | (b,r) <- bs]
+  C.StgTopLifted (C.StgNonRec b r)  -> sequence [visitTopRhs b r]
+  C.StgTopLifted (C.StgRec bs)      -> mapM (uncurry visitTopRhs) bs
 
 visitModule :: C.Module -> CG [Def]
 visitModule C.Module{..} = concat <$> mapM visitTopBinder moduleTopBindings
