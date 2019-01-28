167ef9daaaa83d41ec98537b650c0854fb84aa0c
Csaba Hruska <csaba.hruska@gmail.com>
2019-01-28 18:34:27 +0100

    cleanup

--- lambda-grin/src/Lambda/Lint.hs
+++ lambda-grin/src/Lambda/Lint.hs
@@ -18,73 +18,18 @@ import Grin.Grin (isPrimName)
 import Transformations.Util
 import Lambda.Util
 
-badPrimops :: Set Name
-badPrimops = Set.fromList
-  [ "_ghc_atomicModifyMutVar#"
-  , "_ghc_catch#"
-  , "_ghc_maskAsyncExceptions#"
-  , "_ghc_maskUninterruptible#"
-  , "_ghc_unmaskAsyncExceptions#"
-  , "_ghc_atomically#"
-  , "_ghc_catchRetry#"
-  , "_ghc_catchSTM#"
-  , "_ghc_mkWeak#"
-  , "_ghc_clearCCS#"
-  , "_ghc_broadcast#"
-  , "_ghc_pack#"
-  , "_ghc_unpack#"
-  , "_ghc_insert#"
-  , "_ghc_plus#"
-  , "_ghc_minus#"
-  , "_ghc_times#"
-  , "_ghc_divide#"
-  , "_ghc_quot#"
-  , "_ghc_rem#"
-  , "_ghc_negate#"
-  , "_ghc_indexOffAddr#"
-  , "_ghc_readOffAddr#"
-  , "_ghc_writeOffAddr#"
-  , "_ghc_indexArrayAs#"
-  , "_ghc_readArrayAs#"
-  , "_ghc_writeArrayAs#"
-  , "_ghc_indexOffAddrAs#"
-  , "_ghc_readOffAddrAs#"
-  , "_ghc_writeOffAddrAs#"
-  , "_ghc_indexStablePtrArray#"
-  , "_ghc_indexWord8ArrayAsStablePtr#"
-  , "_ghc_readStablePtrArray#"
-  , "_ghc_readWord8ArrayAsStablePtr#"
-  , "_ghc_indexStablePtrOffAddr#"
-  , "_ghc_readStablePtrOffAddr#"
-  , "_ghc_raise#"
-  , "_ghc_raiseIO#"
-  , "_ghc_retry#"
-  , "_ghc_newMVar#"
-  , "_ghc_finalizeWeak#"
-  , "_ghc_getSpark#"
-  , "_ghc_tagToEnum#"
-  , "_ghc_addrToAny#"
-  , "_ghc_mkApUpd0#"
-  , "_ghc_unpackClosure#"
-  , "_ghc_getApStackVal#"
-  ]
-
 lintLambda :: Program -> IO ()
 lintLambda prg@(Program exts _) = do
   let Env{..} = test prg
       tab = ("  "++) . unpackName
       unknown = Set.difference envUse $ Map.keysSet envDef `Set.union` Set.fromList [eName | External{..} <- exts]
-      unsupported = Set.intersection unknown badPrimops
   --printf "node pats:\n%s" . unlines . map tab $ Set.toList envCon
-  
+
   printf "unknown:\n%s" . unlines . map tab $ Set.toList unknown
   printf "errors:\n%s" . unlines . map tab $ Set.toList envErr
   --printf "unused:\n%s" . unlines . map show $ Set.toList (Set.difference envDef envUse)
   let duplicates = [n | (n,i) <- Map.toList envDef, i > 1]
   printf "duplicates:\n%s" . unlines . map tab $ duplicates
-  case Set.null unsupported of
-    True  -> putStrLn "GHC primop: all ok"
-    False -> printf "unsupported GHC primops:\n%s" . unlines . map tab $ Set.toList unsupported
 
 data Env
   = Env
