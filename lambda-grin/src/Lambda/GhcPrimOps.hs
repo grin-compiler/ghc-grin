0d5054d044a6c491aaba4adfa67be086b27f08cd
Csaba Hruska <csaba.hruska@gmail.com>
2019-03-10 15:05:01 +0100

    clenaup

--- lambda-grin/src/Lambda/GhcPrimOps.hs
+++ lambda-grin/src/Lambda/GhcPrimOps.hs
@@ -22,63 +22,10 @@ isUnboxedPrim name = Set.member name unboxedOps where
 ghcPrimOps = [prog|
 
   grinMain =
-    result_main <- Main.main $ #False
+    result_main <- ":Main.main" $ #"GHC.Prim.realWorld#"
     case result_main of
       (C"GHC.Prim.Unit#" ptr) ->
         _prim_int_print $ 0
       #default ->
         _prim_int_print $ -1
-
-  "_ghc__stg_foreign_call {PlaySafe CCallConv StaticTarget \"_prim_int_print\"}{(# State# RealWorld #)}" i b =
-    _prim_int_print i
-    pure (C"GHC.Prim.(##)")
-
-  "_ghc_negateInt#{Int#}" a =
-    _prim_int_mul -1 a
-
-  "_ghc_*#{Int#}" a b =
-    _prim_int_mul a b
-
-  "_ghc_+#{Int#}" a b =
-    _prim_int_add a b
-
-  "_ghc_-#{Int#}" a b =
-    _prim_int_sub a b
-
-  "_ghc_<#{Int#}" a b =
-    r <- _prim_int_lt a b
-    _ghc_bool_to_int r
-
-  "_ghc_>=#{Int#}" a b =
-    r <- _prim_int_ge a b
-    _ghc_bool_to_int r
-
-  "_ghc_/=#{Int#}" a b =
-    r <- _prim_int_ne a b
-    _ghc_bool_to_int r
-
-  "_ghc_<=#{Int#}" a b =
-    r <- _prim_int_le a b
-    _ghc_bool_to_int r
-
-  "_ghc_==#{Int#}" a b =
-    r <- _prim_int_eq a b
-    _ghc_bool_to_int r
-
-  "_ghc_>#{Int#}" a b =
-    r <- _prim_int_gt a b
-    _ghc_bool_to_int r
-
-  _ghc_bool_to_int a =
-    case a of
-      #False  -> pure 0
-      #True   -> pure 1
-
-  "_ghc_tagToEnum#{Bool}" a =
-    case a of
-      0 -> pure (CGHC.Types.False)
-      1 -> pure (CGHC.Types.True)
-
-  "_ghc_indexIntArray#{Int#}" array index = pure 0 -- TODO
-
 |]
