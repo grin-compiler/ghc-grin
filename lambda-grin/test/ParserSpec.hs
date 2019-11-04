93691d74951dbe75d0ead20907903516cb0e188b
Csaba Hruska <csaba.hruska@gmail.com>
2019-11-04 14:57:59 +0100

    start work on unit tests

---
+++ lambda-grin/test/ParserSpec.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE LambdaCase, QuasiQuotes, OverloadedStrings #-}
+module ParserSpec where
+
+import qualified Data.Text as Text
+import Test.Hspec
+import Test.QuickCheck
+
+import Lambda.TH
+import Lambda.Pretty
+import Lambda.Parse2
+import Lambda.Syntax2
+import Grin.Pretty (PP(..))
+
+runTests :: IO ()
+runTests = hspec spec
+
+spec :: Spec
+spec = do
+  describe "simple" $ do
+    it "case" $ do
+      let before = [prog2|
+        test p =
+          letS
+            x = case p of
+              _ @ alt.1 ->
+                letS
+                  r = #T_Int64 1
+                r
+          x
+        |]
+      let after = Program []
+            [ Def "test" ["p"]
+              ( LetS
+                [ ("x", Case "p"
+                    [ Alt "alt.1" DefaultPat
+                        ( LetS [("r", Lit (LInt64 1)) ] (Var "r"))
+                    ]
+                  )
+                ] (Var "x")
+              )
+            ]
+      (PP before) `shouldBe` (PP after)
