45723e6d3fce5f2c9c0132e0e0e15af1c5fc4943
Csaba Hruska <csaba.hruska@gmail.com>
2020-02-18 19:06:36 +0100

    begin rename ghc-dump-stg to external-stg

---
+++ external-stg/Stg/Convert.hs
@@ -0,0 +1,436 @@
+{-# LANGUAGE RecordWildCards, LambdaCase, TupleSections #-}
+module Stg.Convert where
+
+import GhcPrelude
+
+import qualified Data.ByteString.Char8 as BS8
+
+import qualified CoreSyn      as GHC
+import qualified DataCon      as GHC
+import qualified DynFlags     as GHC
+import qualified FastString   as GHC
+import qualified ForeignCall  as GHC
+import qualified Id           as GHC
+import qualified BasicTypes   as GHC
+import qualified Literal      as GHC
+import qualified Module       as GHC
+import qualified Name         as GHC
+import qualified Outputable   as GHC
+import qualified PrimOp       as GHC
+import qualified TyCon        as GHC
+import qualified Type         as GHC
+import qualified Unique       as GHC
+import qualified GHC.Types.RepType  as GHC
+import qualified GHC.Stg.Syntax     as GHC
+
+import qualified TysWiredIn   as GHC -- TODO: remove this
+
+import Control.Monad
+import Control.Monad.Trans.State.Strict
+import Data.List (foldl')
+import Data.IntMap (IntMap)
+import Data.Maybe (maybe)
+import qualified Data.IntMap as IntMap
+import qualified Data.IntSet as IntSet
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import Stg.Syntax
+
+import Debug.Trace
+import Control.Exception
+import System.IO.Unsafe
+import qualified PprCore as GHC
+
+--trace :: String -> a -> a
+--trace _ = id
+
+-- conversion env
+
+data Env
+  = Env
+  { envExternals  :: IntMap GHC.Id
+  , envAlgTyCons  :: IntMap GHC.TyCon
+  }
+
+emptyEnv :: Env
+emptyEnv = Env
+  { envExternals  = IntMap.empty
+  , envAlgTyCons  = IntMap.empty
+  }
+
+type M = State Env
+
+-- helpers
+
+ppr :: GHC.Outputable a => a -> String
+ppr = GHC.showSDoc GHC.unsafeGlobalDynFlags . GHC.ppr
+
+bs8SDoc :: GHC.SDoc -> BS8.ByteString
+bs8SDoc = BS8.pack . GHC.showSDoc GHC.unsafeGlobalDynFlags
+
+fastStringToByteString :: GHC.FastString -> BS8.ByteString
+fastStringToByteString = GHC.fastStringToByteString
+
+uniqueKey :: GHC.Uniquable a => a -> Int
+uniqueKey = GHC.getKey . GHC.getUnique
+
+cvtUnique :: GHC.Unique -> Unique
+cvtUnique u = Unique a b
+  where (a,b) = GHC.unpkUnique u
+
+-- name conversion
+
+cvtOccName :: GHC.OccName -> Name
+cvtOccName = GHC.fastStringToByteString . GHC.occNameFS
+
+cvtModuleName :: GHC.ModuleName -> ModuleName
+cvtModuleName = ModuleName . fastStringToByteString . GHC.moduleNameFS
+
+-- data con conversion
+
+cvtDataCon :: GHC.DataCon -> M DataConId
+cvtDataCon dc = do
+  let tc    = GHC.dataConTyCon dc
+      tcKey = uniqueKey tc
+  new <- IntMap.notMember tcKey <$> gets envAlgTyCons
+  when new $ modify' $ \m@Env{..} -> m {envAlgTyCons = IntMap.insert tcKey tc envAlgTyCons}
+  pure . DataConId . cvtUnique . GHC.getUnique $ dc
+
+-- type conversion
+
+instance GHC.Outputable Type where
+  ppr = GHC.text . show
+
+trpp :: (GHC.Outputable o, GHC.Outputable a) => String -> (o -> a) -> o -> a
+trpp msg f a = trace (unwords [msg, ":"]) $
+               trace ('\t' : ppr a) $
+               trace (unwords ["\t\t=", ppr (f a), "\n-----------\n"]) $
+               f a
+
+{-# INLINE debugCvtAppType #-}
+debugCvtAppType :: GHC.Id -> [GHC.StgArg] -> GHC.Type -> Type
+debugCvtAppType f args ty = unsafePerformIO $ debugCvtAppTypeM f args ty
+
+{-# INLINE debugCvtAppTypeM #-}
+debugCvtAppTypeM :: GHC.Id -> [GHC.StgArg] -> GHC.Type -> IO Type
+debugCvtAppTypeM f args ty = catch (let t = cvtTypeNormal ty in seq t (pure t)) $ \ex -> do
+  putStrLn $ "cought exception during StgApp result type conversion"
+  putStrLn "function:"
+  putStrLn $ "  " ++ ppr f ++ " :: " ++ ppr (GHC.idType f)
+  putStrLn "args:"
+  forM_ args $ \a -> case a of
+    GHC.StgVarArg o -> putStrLn $ "    " ++ ppr o ++ " :: " ++ ppr (GHC.idType o)
+    GHC.StgLitArg l -> putStrLn $ "    " ++ ppr l ++ " :: " ++ ppr (GHC.literalType l)
+  putStrLn $ "function result type:"
+  putStrLn $ "  " ++ ppr ty
+  throwIO (ex :: SomeException)
+
+cvtType :: String -> GHC.Type -> Type
+--cvtType msg t = trpp (unwords [msg, "cvtType"]) cvtType3 $ deepCheckType t
+cvtType _ = cvtTypeNormal
+
+deepCheckType :: GHC.Type -> GHC.Type
+deepCheckType t = t -- TODO
+
+cvtType3 :: GHC.Type -> Type
+cvtType3 t
+  | trpp "  isTypeLevPoly" GHC.isTypeLevPoly t
+  = PolymorphicRep
+
+  | trpp "  isUnboxedTupleType" GHC.isUnboxedTupleType t
+  = UnboxedTuple []
+
+  | otherwise
+  = SingleValue LiftedRep
+
+{-
+cvtType msg t = trace
+  ( "cvtType - " ++ msg ++
+    " isPiTy: " ++ show (GHC.isPiTy t) ++
+    " isDictLikeTy: " ++ (show $ GHC.isDictLikeTy t) ++
+    " isTypeLevPoly: " ++ (show $ GHC.isTypeLevPoly t) ++
+    " isUnboxedTupleType: " ++ (show $ GHC.isUnboxedTupleType t) ++
+    " isFunTy: " ++ (show $ GHC.isFunTy t) ++
+    " :: " ++ ppr t
+  ) . cvtType2 $ t
+-}
+{-
+isDictLikeTy :: Type -> Bool
+-- Note [Dictionary-like types]
+
+-- | Returns Just True if this type is surely lifted, Just False
+-- if it is surely unlifted, Nothing if we can't be sure (i.e., it is
+-- levity polymorphic), and panics if the kind does not have the shape
+-- TYPE r.
+isLiftedType_maybe :: HasDebugCallStack => Type -> Maybe Bool
+-}
+
+{-# INLINE cvtTypeNormal #-}
+cvtTypeNormal :: GHC.Type -> Type
+cvtTypeNormal t
+  | GHC.isTypeLevPoly t
+  = PolymorphicRep
+
+  -- TODO: remove this test code case (only for an experiment)
+  ---------------- begin -------
+  | GHC.isUnboxedSumType t
+  = error $ "unboxed sum are not supported yet! " ++ ppr t
+  ---------------- end -------
+
+  | GHC.isUnboxedTupleType t
+  = UnboxedTuple (map cvtPrimRep $ GHC.typePrimRepArgs t)
+
+  | [rep] <- GHC.typePrimRepArgs t
+  = SingleValue (cvtPrimRep rep)
+
+  | otherwise
+  = error $ "could not convert type: " ++ ppr t
+
+cvtPrimRep :: GHC.PrimRep -> PrimRep
+cvtPrimRep = \case
+  GHC.VoidRep     -> VoidRep
+  GHC.LiftedRep   -> LiftedRep
+  GHC.UnliftedRep -> UnliftedRep
+  GHC.Int8Rep     -> Int8Rep
+  GHC.Int16Rep    -> Int16Rep
+  GHC.Int32Rep    -> Int32Rep
+  GHC.Int64Rep    -> Int64Rep
+  GHC.IntRep      -> IntRep
+  GHC.Word8Rep    -> Word8Rep
+  GHC.Word16Rep   -> Word16Rep
+  GHC.Word32Rep   -> Word32Rep
+  GHC.Word64Rep   -> Word64Rep
+  GHC.WordRep     -> WordRep
+  GHC.AddrRep     -> AddrRep
+  GHC.FloatRep    -> FloatRep
+  GHC.DoubleRep   -> DoubleRep
+  GHC.VecRep i e  -> VecRep i $ cvtPrimElemRep e
+
+cvtPrimElemRep :: GHC.PrimElemRep -> PrimElemRep
+cvtPrimElemRep = \case
+  GHC.Int8ElemRep   -> Int8ElemRep
+  GHC.Int16ElemRep  -> Int16ElemRep
+  GHC.Int32ElemRep  -> Int32ElemRep
+  GHC.Int64ElemRep  -> Int64ElemRep
+  GHC.Word8ElemRep  -> Word8ElemRep
+  GHC.Word16ElemRep -> Word16ElemRep
+  GHC.Word32ElemRep -> Word32ElemRep
+  GHC.Word64ElemRep -> Word64ElemRep
+  GHC.FloatElemRep  -> FloatElemRep
+  GHC.DoubleElemRep -> DoubleElemRep
+
+-- literal conversion
+
+cvtLitNumType :: GHC.LitNumType -> LitNumType
+cvtLitNumType = \case
+  GHC.LitNumInteger -> LitNumInteger
+  GHC.LitNumNatural -> LitNumNatural
+  GHC.LitNumInt     -> LitNumInt
+  GHC.LitNumInt64   -> LitNumInt64
+  GHC.LitNumWord    -> LitNumWord
+  GHC.LitNumWord64  -> LitNumWord64
+
+cvtLabelSpec :: Maybe Int -> GHC.FunctionOrData -> LabelSpec
+cvtLabelSpec mi = \case
+  GHC.IsFunction  -> FunctionLabel mi
+  GHC.IsData      -> DataLabel
+
+cvtLit :: GHC.Literal -> Lit
+cvtLit = \case
+  GHC.LitChar x       -> LitChar x
+  GHC.LitString x     -> LitString x
+  GHC.LitNullAddr     -> LitNullAddr
+  GHC.LitFloat x      -> LitFloat x
+  GHC.LitDouble x     -> LitDouble x
+  GHC.LitLabel x i d  -> LitLabel (fastStringToByteString  x) (cvtLabelSpec i d)
+  GHC.LitNumber t i _ -> LitNumber (cvtLitNumType t) i
+
+-- Id conversion
+
+mkBinderId :: GHC.Uniquable a => a -> BinderId
+mkBinderId = BinderId . cvtUnique . GHC.getUnique
+
+cvtOccId :: GHC.Id -> M BinderId
+cvtOccId x = do
+  let name = GHC.getName x
+  when (GHC.isExternalName name) $ do
+    let key = uniqueKey x
+    new <- IntMap.notMember key <$> gets envExternals
+    when new $ modify' $ \m@Env{..} -> m {envExternals = IntMap.insert key x envExternals}
+  pure $ mkBinderId x
+
+cvtBinderIdClosureParam :: String -> GHC.Id -> SBinder
+cvtBinderIdClosureParam msg v
+  | GHC.isId v = SBinder
+      { sbinderName     = cvtOccName $ GHC.getOccName v
+      , sbinderId       = BinderId . cvtUnique . GHC.idUnique $ v
+      , sbinderType     = SingleValue . cvtPrimRep . {-trpp (unwords [msg, "cvtBinderIdClosureParam", ppr v])-} GHC.typePrimRep1 $ GHC.idType v
+      , sbinderTypeSig  = BS8.pack . ppr $ GHC.idType v
+      }
+  | otherwise = error $ "Type binder in STG: " ++ (show $ cvtOccName $ GHC.getOccName v)
+
+
+cvtBinderId :: String -> GHC.Id -> SBinder
+cvtBinderId msg v
+  | GHC.isId v = SBinder
+      { sbinderName     = cvtOccName $ GHC.getOccName v
+      , sbinderId       = BinderId . cvtUnique . GHC.idUnique $ v
+      , sbinderType     = cvtType (unwords [msg, "cvtBinderId", ppr v]) $ GHC.idType v
+      , sbinderTypeSig  = BS8.pack . ppr $ GHC.idType v
+      }
+  | otherwise = error $ "Type binder in STG: " ++ (show $ cvtOccName $ GHC.getOccName v)
+
+-- stg op conversion
+
+cvtCCallTarget :: GHC.CCallTarget -> CCallTarget
+cvtCCallTarget = \case
+  GHC.StaticTarget _ t _ _  -> StaticTarget $ fastStringToByteString t
+  GHC.DynamicTarget         -> DynamicTarget
+
+cvtCCallConv :: GHC.CCallConv -> CCallConv
+cvtCCallConv = \case
+  GHC.CCallConv           -> CCallConv
+  GHC.CApiConv            -> CApiConv
+  GHC.StdCallConv         -> StdCallConv
+  GHC.PrimCallConv        -> PrimCallConv
+  GHC.JavaScriptCallConv  -> JavaScriptCallConv
+
+cvtSafety :: GHC.Safety -> Safety
+cvtSafety = \case
+  GHC.PlaySafe          -> PlaySafe
+  GHC.PlayInterruptible -> PlayInterruptible
+  GHC.PlayRisky         -> PlayRisky
+
+cvtForeignCall :: GHC.ForeignCall -> ForeignCall
+cvtForeignCall (GHC.CCall (GHC.CCallSpec t c s)) = ForeignCall (cvtCCallTarget t) (cvtCCallConv c) (cvtSafety s)
+
+cvtOp :: GHC.StgOp -> StgOp
+cvtOp = \case
+  GHC.StgPrimOp o     -> StgPrimOp (cvtOccName $ GHC.primOpOcc o)
+  GHC.StgPrimCallOp _ -> StgPrimCallOp PrimCall -- TODO
+  GHC.StgFCallOp f _  -> StgFCallOp $ cvtForeignCall f
+
+-- arg conversion
+
+cvtArg :: GHC.StgArg -> M SArg
+cvtArg = \case
+  GHC.StgVarArg o -> StgVarArg <$> cvtOccId o
+  GHC.StgLitArg l -> pure $ StgLitArg (cvtLit l)
+
+-- alt conversion
+
+cvtAlt :: GHC.StgAlt -> M SAlt
+cvtAlt (con, bs, e) = Alt <$> cvtAltCon con <*> pure (map (cvtBinderId "Alt") bs) <*> cvtExpr e
+
+cvtAltCon :: GHC.AltCon -> M SAltCon
+cvtAltCon = \case
+  GHC.DataAlt con -> AltDataCon <$> cvtDataCon con
+  GHC.LitAlt l    -> pure . AltLit $ cvtLit l
+  GHC.DEFAULT     -> pure $ AltDefault
+
+-- stg expr conversion
+
+cvtExpr :: GHC.StgExpr -> M SExpr
+cvtExpr = \case
+  GHC.StgApp f ps (tr,o)  -> StgApp <$> cvtOccId f <*> mapM cvtArg ps <*> pure ({-cvtType "StgApp" t-}debugCvtAppType f ps tr) <*> pure (BS8.pack . ppr $ GHC.idType f, BS8.pack $ ppr tr, BS8.pack o)
+  GHC.StgLit l              -> pure $ StgLit (cvtLit l)
+  GHC.StgConApp dc ps ts    -> StgConApp <$> cvtDataCon dc <*> mapM cvtArg ps <*> pure (map (cvtType "StgConApp") ts)
+  GHC.StgOpApp o ps t       -> StgOpApp (cvtOp o) <$> mapM cvtArg ps <*> pure (cvtType "StgOpApp" t)
+  GHC.StgCase e b _ al      -> StgCase <$> cvtExpr e <*> pure (cvtBinderId "StgCase" b) <*> mapM cvtAlt al
+  GHC.StgLet _ b e          -> StgLet <$> cvtBind b <*> cvtExpr e
+  GHC.StgLetNoEscape _ b e  -> StgLetNoEscape <$> cvtBind b <*> cvtExpr e
+  GHC.StgTick _ e           -> cvtExpr e
+  e                         -> error $ "invalid stg expression: " ++ ppr e
+
+-- stg rhs conversion (heap objects)
+
+cvtUpdateFlag :: GHC.UpdateFlag -> UpdateFlag
+cvtUpdateFlag = \case
+  GHC.ReEntrant   -> ReEntrant
+  GHC.Updatable   -> Updatable
+  GHC.SingleEntry -> SingleEntry
+
+cvtRhs :: GHC.StgRhs -> M SRhs
+cvtRhs = \case
+  GHC.StgRhsClosure _ _ u bs e  -> StgRhsClosure (cvtUpdateFlag u) (map (cvtBinderIdClosureParam "StgRhsClosure") bs) <$> cvtExpr e
+  GHC.StgRhsCon _ dc args       -> StgRhsCon <$> cvtDataCon dc <*> mapM cvtArg args
+
+-- bind and top-bind conversion
+
+cvtBind :: GHC.StgBinding -> M SBinding
+cvtBind = \case
+  GHC.StgNonRec b r -> StgNonRec (cvtBinderId "StgNonRec" b) <$> cvtRhs r
+  GHC.StgRec    bs  -> StgRec <$> sequence [(cvtBinderId "StgRec" b,) <$> cvtRhs r | (b, r) <- bs]
+
+cvtTopBind :: GHC.StgTopBinding -> M STopBinding
+cvtTopBind = \case
+  GHC.StgTopLifted b        -> StgTopLifted <$> cvtBind b
+  GHC.StgTopStringLit b bs  -> pure $ StgTopStringLit (cvtBinderId "StgTopStringLit" b) bs
+
+cvtTopBinds :: [GHC.StgTopBinding] -> M [STopBinding]
+cvtTopBinds binds = mapM cvtTopBind binds
+
+-- module conversion
+
+cvtModule :: GHC.CoreProgram -> GHC.CoreProgram -> String -> GHC.ModuleName -> [GHC.StgTopBinding] -> SModule
+cvtModule core prep_core phase modName' binds =
+  Module
+  { modulePhase           = BS8.pack phase
+  , moduleName            = modName
+  , moduleDependency      = Set.toList . Set.delete modName . Set.fromList . map fst $ externals
+  , moduleExternalTopIds  = externals
+  , moduleAlgTyCons       = algTyCons
+  , moduleExported        = exported
+  , moduleTopBindings     = topBinds
+  , moduleCoreSrc         = bs8SDoc $ GHC.pprCoreBindings core
+  , modulePrepCoreSrc     = bs8SDoc $ GHC.pprCoreBindings prep_core
+  } where
+      (topBinds, Env{..}) = runState (cvtTopBinds binds) emptyEnv
+      stgTopIds           = concatMap topBindIds binds
+      topKeys             = IntSet.fromList $ map uniqueKey stgTopIds
+      exported            = groupByModule [ (maybe modName (cvtModuleName . GHC.moduleName) $ GHC.nameModule_maybe $ GHC.getName i, mkBinderId i)
+                                          | i <- stgTopIds, GHC.isExportedId i]
+      modName             = cvtModuleName modName'
+      externals           = groupByModule [mkExternalName e | (k,e) <- IntMap.toList envExternals, IntSet.notMember k topKeys]
+      algTyCons           = groupByModule . map mkAlgTyCon $ IntMap.elems envAlgTyCons
+
+-- utils
+
+groupByModule :: [(ModuleName, b)] -> [(ModuleName, [b])]
+groupByModule = Map.toList . foldl' (\a (m,b) -> Map.insertWith (++) m [b] a) Map.empty
+
+mkExternalName :: GHC.Id -> (ModuleName, SBinder)
+mkExternalName x = (cvtModuleName $ GHC.moduleName $ GHC.nameModule $ GHC.getName x, cvtBinderId "mkExternalName" x)
+
+mkAlgTyCon :: GHC.TyCon -> (ModuleName, AlgTyCon)
+mkAlgTyCon tc = (cvtModuleName $ GHC.moduleName $ GHC.nameModule x, b) where
+  x = GHC.getName tc
+  b = AlgTyCon
+      { tcName      = cvtOccName $ GHC.getOccName x
+      , tcDataCons  = map mkSDataCon . sortDataCons $ GHC.tyConDataCons tc
+      }
+  sortDataCons l = IntMap.elems $ IntMap.fromList [(GHC.dataConTag dc, dc) | dc <- l]
+
+mkSDataCon :: GHC.DataCon -> SDataCon
+mkSDataCon dc = SDataCon
+  { sdcName   = cvtOccName $ GHC.getOccName n
+  , sdcId     = DataConId . cvtUnique . GHC.getUnique $ n
+  , sdcModule = cvtModuleName $ GHC.moduleName $ GHC.nameModule n
+  , sdcRep    = if GHC.isUnboxedTupleCon dc
+                  then UnboxedDataCon
+                  else AlgDataCon $ map (getConArgRep . dcpp "3" GHC.typePrimRepArgs) $ dcpp "2" GHC.dataConRepArgTys $ dcpp "1" id $ dc
+  } where
+      dcpp :: GHC.Outputable o => String -> (o -> a) -> o -> a
+      dcpp _ f x = f x
+      --dcpp msg f a = trace ("mkSDataCon " ++ msg ++ " : " ++ ppr a) $ f a
+      n = GHC.getName dc
+      getConArgRep = \case
+        [r] -> cvtPrimRep r
+        r   -> error $ "data con " ++ ppr n ++ "has invalid argument representation: " ++ ppr r
+
+topBindIds :: GHC.StgTopBinding -> [GHC.Id]
+topBindIds = \case
+  GHC.StgTopLifted (GHC.StgNonRec b _)  -> [b]
+  GHC.StgTopLifted (GHC.StgRec bs)      ->  map fst bs
+  GHC.StgTopStringLit b _               -> [b]
---
+++ external-stg/Stg/Syntax.hs
@@ -0,0 +1,352 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+module Stg.Syntax where
+
+import GhcPrelude
+
+import GHC.Generics
+
+import Data.Monoid
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import Data.Binary
+
+-- data types
+
+type Name = BS8.ByteString
+
+data Unique
+  = Unique !Char !Int
+  deriving (Eq, Ord, Generic)
+
+instance Show Unique where
+ show (Unique c n) = c : show n
+
+-- type related
+
+data UnliftedSpec
+  = UnliftedAlgTyCon
+  | UnliftedPrimType  Name
+
+data PrimRep
+  = VoidRep
+  | LiftedRep
+  | UnliftedRep   -- ^ Unlifted pointer
+  | Int8Rep       -- ^ Signed, 8-bit value
+  | Int16Rep      -- ^ Signed, 16-bit value
+  | Int32Rep      -- ^ Signed, 32-bit value
+  | Int64Rep      -- ^ Signed, 64 bit value (with 32-bit words only)
+  | IntRep        -- ^ Signed, word-sized value
+  | Word8Rep      -- ^ Unsigned, 8 bit value
+  | Word16Rep     -- ^ Unsigned, 16 bit value
+  | Word32Rep     -- ^ Unsigned, 32 bit value
+  | Word64Rep     -- ^ Unsigned, 64 bit value (with 32-bit words only)
+  | WordRep       -- ^ Unsigned, word-sized value
+  | AddrRep       -- ^ A pointer, but /not/ to a Haskell value (use '(Un)liftedRep')
+  | FloatRep
+  | DoubleRep
+  | VecRep Int PrimElemRep  -- ^ A vector
+  deriving (Eq, Ord, Generic, Show)
+
+data PrimElemRep
+  = Int8ElemRep
+  | Int16ElemRep
+  | Int32ElemRep
+  | Int64ElemRep
+  | Word8ElemRep
+  | Word16ElemRep
+  | Word32ElemRep
+  | Word64ElemRep
+  | FloatElemRep
+  | DoubleElemRep
+  deriving (Eq, Ord, Generic, Show)
+
+
+{-
+  Q: do we want to keep haskell types OR would representation type system be enough?
+  A: keep only those information that is relevant for the codegen
+
+  HINT: extrenal STG and lambda IR should be identical
+-}
+
+data Type
+  = SingleValue     PrimRep
+  | UnboxedTuple    [PrimRep]
+  | PolymorphicRep
+  deriving (Eq, Ord, Generic, Show)
+
+-- data con related
+
+newtype DataConId
+  = DataConId Unique
+  deriving (Eq, Ord, Binary, Show)
+
+-- raw data con
+data SDataCon
+  = SDataCon
+  { sdcName   :: !Name
+  , sdcId     :: !DataConId
+  , sdcModule :: !ModuleName
+  , sdcRep    :: DataConRep
+  }
+  deriving (Eq, Ord, Generic, Show)
+
+data DataConRep
+  = AlgDataCon      ![PrimRep]
+  | UnboxedDataCon
+  deriving (Eq, Ord, Generic, Show)
+
+-- user friendly data con
+data DataCon
+  = DataCon
+  { dcName   :: !Name
+  , dcId     :: !DataConId
+  , dcModule :: !ModuleName
+  , dcRep    :: !DataConRep
+  , dcTyCon  :: !AlgTyCon
+  }
+  deriving (Eq, Ord, Generic, Show)
+
+data AlgTyCon
+  = AlgTyCon
+  { tcName      :: !Name
+  , tcDataCons  :: ![SDataCon]
+  }
+  deriving (Eq, Ord, Generic, Show)
+
+-- stg expr related
+
+newtype ModuleName
+  = ModuleName { getModuleName :: Name }
+  deriving (Eq, Ord, Binary, Show)
+
+newtype BinderId
+  = BinderId Unique
+  deriving (Eq, Ord, Binary, Show)
+
+data SBinder
+  = SBinder
+    { sbinderName     :: !Name
+    , sbinderId       :: !BinderId
+    , sbinderType     :: !Type
+    , sbinderTypeSig  :: !Name
+    }
+  deriving (Eq, Ord, Generic, Show)
+
+data Binder
+  = Binder
+    { binderName        :: !Name
+    , binderId          :: !BinderId
+    , binderType        :: !Type
+    , binderTypeSig     :: !Name
+    , binderModule      :: !ModuleName
+    , binderIsTop       :: !Bool
+    , binderIsExported  :: !Bool
+    }
+  deriving (Eq, Ord, Generic, Show)
+
+binderUniqueName :: Binder -> Name
+binderUniqueName Binder{..}
+  | binderIsExported  = getModuleName binderModule <> BS8.pack "." <> binderName
+  | otherwise         = binderName <> BS8.pack ('.' : show u)
+  where BinderId u = binderId
+
+data LitNumType
+  = LitNumInteger -- ^ @Integer@ (see Note [Integer literals])
+  | LitNumNatural -- ^ @Natural@ (see Note [Natural literals])
+  | LitNumInt     -- ^ @Int#@ - according to target machine
+  | LitNumInt64   -- ^ @Int64#@ - exactly 64 bits
+  | LitNumWord    -- ^ @Word#@ - according to target machine
+  | LitNumWord64  -- ^ @Word64#@ - exactly 64 bits
+  deriving (Eq, Ord, Generic, Show)
+
+data LabelSpec
+  = FunctionLabel (Maybe Int) -- only for stdcall convention
+  | DataLabel
+  deriving (Eq, Ord, Generic, Show)
+
+data Lit
+  = LitChar     Char
+  | LitString   BS.ByteString
+  | LitNullAddr
+  | LitFloat    Rational
+  | LitDouble   Rational
+  | LitLabel    BS8.ByteString LabelSpec
+  | LitNumber   LitNumType Integer
+  deriving (Eq, Ord, Generic, Show)
+
+-- | A top-level binding.
+data TopBinding' idBnd idOcc dcOcc
+-- See Note [CoreSyn top-level string literals]
+  = StgTopLifted    (Binding' idBnd idOcc dcOcc)
+  | StgTopStringLit idBnd BS.ByteString
+  deriving (Eq, Ord, Generic, Show)
+
+data Binding' idBnd idOcc dcOcc
+  = StgNonRec idBnd (Rhs' idBnd idOcc dcOcc)
+  | StgRec    [(idBnd, Rhs' idBnd idOcc dcOcc)]
+  deriving (Eq, Ord, Generic, Show)
+
+data Arg' idOcc
+  = StgVarArg  idOcc
+  | StgLitArg  Lit
+  deriving (Eq, Ord, Generic, Show)
+
+data Expr' idBnd idOcc dcOcc
+  = StgApp
+        idOcc         -- function
+        [Arg' idOcc]  -- arguments; may be empty
+        Type          -- result type
+        (Name,Name,Name)  -- fun core type pp, result core type pp, StgApp oigin (Var/Coercion/App)
+
+  | StgLit      Lit
+
+        -- StgConApp is vital for returning unboxed tuples or sums
+        -- which can't be let-bound first
+  | StgConApp   dcOcc         -- DataCon
+                [Arg' idOcc]  -- Saturated
+                [Type]        -- types
+
+  | StgOpApp    StgOp         -- Primitive op or foreign call
+                [Arg' idOcc]  -- Saturated.
+                Type          -- result type
+
+  | StgCase
+        (Expr' idBnd idOcc dcOcc)     -- the thing to examine
+
+        idBnd                         -- binds the result of evaluating the scrutinee
+
+        [Alt' idBnd idOcc dcOcc]      -- The DEFAULT case is always *first*
+                                      -- if it is there at all
+
+  | StgLet
+        (Binding' idBnd idOcc dcOcc)  -- right hand sides (see below)
+        (Expr' idBnd idOcc dcOcc)     -- body
+
+  | StgLetNoEscape
+        (Binding' idBnd idOcc dcOcc)  -- right hand sides (see below)
+        (Expr' idBnd idOcc dcOcc)     -- body
+  deriving (Eq, Ord, Generic, Show)
+
+
+data UpdateFlag = ReEntrant | Updatable | SingleEntry
+  deriving (Eq, Ord, Generic, Show)
+
+data Rhs' idBnd idOcc dcOcc
+  = StgRhsClosure
+        !UpdateFlag               -- ReEntrant | Updatable | SingleEntry
+        [idBnd]                   -- arguments; if empty, then not a function;
+                                  -- as above, order is important.
+        (Expr' idBnd idOcc dcOcc) -- body
+
+  | StgRhsCon
+        dcOcc  -- DataCon
+        [Arg' idOcc]        -- Args
+  deriving (Eq, Ord, Generic, Show)
+
+data Alt' idBnd idOcc dcOcc
+  = Alt
+    { altCon     :: !(AltCon' dcOcc)
+    , altBinders :: [idBnd]
+    , altRHS     :: Expr' idBnd idOcc dcOcc
+    }
+  deriving (Eq, Ord, Generic, Show)
+
+data AltCon' dcOcc
+  = AltDataCon  dcOcc
+  | AltLit      Lit
+  | AltDefault
+  deriving (Eq, Ord, Generic, Show)
+
+data Safety = PlaySafe | PlayInterruptible | PlayRisky
+  deriving (Eq, Ord, Generic, Show)
+
+data CCallConv = CCallConv | CApiConv | StdCallConv | PrimCallConv | JavaScriptCallConv
+  deriving (Eq, Ord, Generic, Show)
+
+data CCallTarget
+  = StaticTarget BS8.ByteString
+  | DynamicTarget
+  deriving (Eq, Ord, Generic, Show)
+
+data ForeignCall
+  = ForeignCall
+  { foreignCTarget  :: CCallTarget
+  , foreignCConv    :: CCallConv
+  , foreignCSafety  :: Safety
+  }
+  deriving (Eq, Ord, Generic, Show)
+
+data PrimCall = PrimCall -- BS8.ByteString BS8.ByteString
+  deriving (Eq, Ord, Generic, Show)
+
+data StgOp
+  = StgPrimOp     Name
+  | StgPrimCallOp PrimCall
+  | StgFCallOp    ForeignCall
+  deriving (Eq, Ord, Generic, Show)
+
+data Module' idBnd idOcc dcOcc
+  = Module
+  { modulePhase           :: !BS8.ByteString
+  , moduleName            :: !ModuleName
+  , moduleDependency      :: ![ModuleName]
+  , moduleExternalTopIds  :: ![(ModuleName, [idBnd])]
+  , moduleAlgTyCons       :: ![(ModuleName, [AlgTyCon])]
+  , moduleExported        :: ![(ModuleName, [BinderId])]
+  , moduleTopBindings     :: ![TopBinding' idBnd idOcc dcOcc]
+  , moduleCoreSrc         :: !BS8.ByteString
+  , modulePrepCoreSrc     :: !BS8.ByteString
+  }
+  deriving (Eq, Ord, Generic, Show)
+
+-- convenience layers: raw and user friendly
+
+-- raw - as it is serialized
+type STopBinding = TopBinding'  SBinder BinderId DataConId
+type SBinding    = Binding'     SBinder BinderId DataConId
+type SExpr       = Expr'        SBinder BinderId DataConId
+type SRhs        = Rhs'         SBinder BinderId DataConId
+type SAlt        = Alt'         SBinder BinderId DataConId
+type SModule     = Module'      SBinder BinderId DataConId
+type SAltCon     = AltCon'      DataConId
+type SArg        = Arg'         BinderId
+
+-- user friendly - rich information
+type TopBinding = TopBinding'  Binder Binder DataCon
+type Binding    = Binding'     Binder Binder DataCon
+type Expr       = Expr'        Binder Binder DataCon
+type Rhs        = Rhs'         Binder Binder DataCon
+type Alt        = Alt'         Binder Binder DataCon
+type Module     = Module'      Binder Binder DataCon
+type AltCon     = AltCon'      DataCon
+type Arg        = Arg'         Binder
+
+instance Binary Unique
+instance Binary PrimElemRep
+instance Binary PrimRep
+instance Binary Type
+instance Binary Binder
+instance Binary SBinder
+instance Binary LitNumType
+instance Binary LabelSpec
+instance Binary Lit
+instance Binary CCallTarget
+instance Binary CCallConv
+instance Binary Safety
+instance Binary ForeignCall
+instance Binary PrimCall
+instance Binary UpdateFlag
+instance Binary StgOp
+instance Binary DataConRep
+instance Binary SDataCon
+instance Binary AlgTyCon
+instance (Binary dcOcc) => Binary (AltCon' dcOcc)
+instance (Binary idOcc) => Binary (Arg' idOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (TopBinding' idBnd idOcc dcOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (Binding' idBnd idOcc dcOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (Expr' idBnd idOcc dcOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (Rhs' idBnd idOcc dcOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (Alt' idBnd idOcc dcOcc)
+instance (Binary idBnd, Binary idOcc, Binary dcOcc) => Binary (Module' idBnd idOcc dcOcc)
