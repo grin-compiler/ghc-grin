5cee10c687e02b5d76bc452691c6becf495ffc8f
Csaba Hruska <csaba.hruska@gmail.com>
2020-03-06 16:01:37 +0100

    implement dead ADT elimination

--- lambda-grin/src/Lambda/DeadFunctionEliminationM.hs
+++ lambda-grin/src/Lambda/DeadFunctionEliminationM.hs
@@ -22,7 +22,7 @@ import Lambda.Syntax
 import Transformations.Util
 
 deadFunctionEliminationM :: [String] -> Program -> IO Program
-deadFunctionEliminationM liveSources prg@(Program exts cons sdata defs) = do
+deadFunctionEliminationM liveSources prg@Program{..} = do
 
   putStrLn "export facts"
   tmpSys <- getCanonicalTemporaryDirectory
@@ -45,7 +45,7 @@ deadFunctionEliminationM liveSources prg@(Program exts cons sdata defs) = do
   closeFact factConRef
 
   factConGroup <- openFact "ConGroup.facts"
-  mapM_ (collectConsM factConGroup) cons
+  mapM_ (collectConsM factConGroup) pConstructors
   closeFact factConGroup
 
   let srcFile = tmpDfe </> "LiveSource.facts"
@@ -58,15 +58,18 @@ deadFunctionEliminationM liveSources prg@(Program exts cons sdata defs) = do
 
   -- read output fatcs
   putStrLn "read back result"
-  liveDefSet <- Set.fromList . map (packName . Text.unpack) . Text.lines <$> Text.readFile (tmpDfe </> "LiveDefName.csv")
   liveConGroupSet <- Set.fromList . map (packName . Text.unpack) . Text.lines <$> Text.readFile (tmpDfe </> "LiveGroupName.csv")
+  liveDefNames <- map (packName . Text.unpack) . Text.lines <$> Text.readFile (tmpDfe </> "LiveDefName.csv")
+  let liveDefSet = Set.fromList liveDefNames
 
-  let liveExts  = [e | e <- exts, Set.member (eName e) liveDefSet]
-      liveDefs  = [d | d@(Def name _ _) <- defs, Set.member name liveDefSet]
-      liveSData = [d | d <- sdata, Set.member (sName d) liveDefSet]
-      liveCons  = [c | c <- cons, Set.member (cgName c) liveConGroupSet]
-
-  pure (Program liveExts liveCons liveSData liveDefs)
+  -- result program
+  pure Program
+    { pExternals    = [e | e <- pExternals, Set.member (eName e) liveDefSet]
+    , pConstructors = [c | c <- pConstructors, Set.member (cgName c) liveConGroupSet]
+    , pPublicNames  = [n | n <- pPublicNames, Set.member n liveDefSet]
+    , pStaticData   = [d | d <- pStaticData, Set.member (sName d) liveDefSet]
+    , pDefinitions  = [d | d@(Def name _ _) <- pDefinitions, Set.member name liveDefSet]
+    }
 
 collectConsM :: Handle -> ConGroup -> IO ()
 collectConsM h ConGroup{..} = do
@@ -74,15 +77,15 @@ collectConsM h ConGroup{..} = do
     hPutStrLn h $ unpackName cgName <> "\t" <> unpackName csName
 
 collectNamesM :: Handle -> Handle -> Program -> IO ()
-collectNamesM hDef hCon (Program exts _cons sdata defs) = do
+collectNamesM hDef hCon Program{..} = do
   let defSet :: Set Name
-      defSet = Set.fromList [name | (Def name _ _) <- defs]
+      defSet = Set.fromList [name | (Def name _ _) <- pDefinitions]
 
       extSet :: Set Name
-      extSet = Set.fromList [eName e | e <- exts]
+      extSet = Set.fromList [eName e | e <- pExternals]
 
       sdataSet :: Set Name
-      sdataSet = Set.fromList [sName s | s <- sdata]
+      sdataSet = Set.fromList [sName s | s <- pStaticData]
 
       nameSet :: Set Name
       nameSet = mconcat [defSet, extSet, sdataSet]
@@ -105,5 +108,5 @@ collectNamesM hDef hCon (Program exts _cons sdata defs) = do
         when (Set.member n nameSet) $ do
           hPutStrLn hDef $ defName <> "\t" <> unpackName n
 
-  forM_ defs $ \def@(Def name _ _) -> do
+  forM_ pDefinitions $ \def@(Def name _ _) -> do
     cataM (folder (unpackName name)) def
