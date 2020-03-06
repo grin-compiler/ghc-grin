6a6517e4196a1eb04cafc5a609830b28b120a061
Csaba Hruska <csaba.hruska@gmail.com>
2020-03-06 15:59:38 +0100

    simplify ext-stg cli tool

--- external-stg-util/app/Main.hs
+++ external-stg-util/app/Main.hs
@@ -11,28 +11,10 @@ import Text.PrettyPrint.ANSI.Leijen hiding ((<$>), (<>))
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import qualified Data.ByteString.Char8 as BS8
 
-import Text.Regex.TDFA
-import Text.Regex.TDFA.Common (Regex)
-import Text.Regex.TDFA.Text
-
 import Stg.Syntax
 import Stg.Pretty
 import Stg.Util
 
-
-filterBindings :: Regex -> Module -> Module
-filterBindings re m =
-    m { moduleTopBindings = mapMaybe filterTopBinding $ moduleTopBindings m }
-  where
-    filterTopBinding bn = case bn of
-      StgTopStringLit b _           | nameMatches b   -> Just bn
-      StgTopLifted (StgNonRec b _)  | nameMatches b   -> Just bn
-      StgTopLifted (StgRec bs)      | not $ null bs'  -> Just . StgTopLifted . StgRec $ bs'
-        where bs' = filter (\(b,_) -> nameMatches b) bs
-      _                                               -> Nothing
-
-    nameMatches :: Binder -> Bool
-    nameMatches b = matchTest re (binderUniqueName b)
 
 modes :: Parser (IO ())
 modes = subparser
@@ -45,22 +27,14 @@ modes = subparser
     mode name f opts = command name (info (helper <*> f) opts)
 
     dumpFile :: Parser FilePath
-    dumpFile = argument str (metavar "DUMP FILE" <> help "CBOR dump file")
-
-    filterCond :: Parser (Module -> Module)
-    filterCond =
-        fmap (maybe id filterBindings)
-        $ option (str >>= fmap Just . makeRegexM')
-                 (short 'f' <> long "filter" <> value Nothing <> help "filter bindings by name")
-      where
-        makeRegexM' = makeRegexM :: String -> ReadM Regex
+    dumpFile = argument str (metavar "DUMP FILE" <> help "STGBIN dump file")
 
     showMode :: Parser (IO ())
     showMode =
-        run <$> filterCond <*> dumpFile
+        run <$> dumpFile
       where
-        run filterFn fname = do
-            dump <- filterFn <$> Stg.Util.readDump fname
+        run fname = do
+            dump <- Stg.Util.readDump fname
             print $ pprModule dump
 
     showPCoreMode :: Parser (IO ())
