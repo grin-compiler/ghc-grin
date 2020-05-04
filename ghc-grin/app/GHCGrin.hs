c192f3a2e5faca8d2ce45b1e535bb51908538de4
Csaba Hruska <csaba.hruska@gmail.com>
2020-05-04 09:19:48 +0200

    collect foreign stubs across program modules

--- ghc-grin/app/GHCGrin.hs
+++ ghc-grin/app/GHCGrin.hs
@@ -10,7 +10,9 @@ import System.Environment
 import System.Exit
 import System.IO
 
-import Stg.Syntax (ModuleName(..), UnitId(..))
+import System.FilePath.Find
+
+import Stg.Syntax (ModuleName(..), UnitId(..), ForeignStubs(..))
 import Stg.Util
 
 import Lambda.GHCSymbols as GHCSymbols
@@ -25,7 +27,7 @@ import Lambda.DeadFunctionEliminationM
 --import Pipeline.Pipeline
 
 import Data.Maybe
-import Data.List (foldl')
+import Data.List (foldl', isPrefixOf)
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 import qualified Data.ByteString.Char8 as BS8
@@ -41,30 +43,34 @@ import System.Posix.Resource
 
 data Opts
   = Opts
-  { inputs :: [FilePath]
+  { input  :: FilePath
   , output :: FilePath
   }
 
-showUsage = do putStrLn "Usage: ghc-grin <stg-files> [-o <output-file>]"
+showUsage = do putStrLn "Usage: ghc-grin <program.stgapp> [-o <output-file>]"
                exitWith ExitSuccess
 
 getOpts :: IO Opts
 getOpts = do xs <- getArgs
-             return $ process (Opts [] "a.out") xs
+             return $ process (Opts [] "") xs
   where
     process opts ("-o":o:xs) = process (opts { output = o }) xs
-    process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
-    process opts [] = opts
+    process opts (x:xs) = process (opts { input = x }) xs
+    process opts [] = opts {output = if output opts == "" then input opts else output opts}
 
 deriving instance Show ResourceLimit
 deriving instance Show ResourceLimits
 
-collectImportedModules :: [FilePath] -> String -> String -> IO [FilePath]
+{-
+readDumpInfo :: FilePath -> IO (Name, UnitId, ModuleName, ForeignStubs, Bool, [(UnitId, [ModuleName])])
+-}
+collectImportedModules :: [FilePath] -> String -> String -> IO ([ForeignStubs], [FilePath])
 collectImportedModules stgbinFileNames unitId mod = do
   -- filter dependenies only
-  depList <- forM stgbinFileNames $ \fname -> do
-    (_, u, m, deps) <- readDumpInfo fname
-    pure ((u, m), [(du, dm) | (du, dl) <- deps, dm <- dl])
+  (stubs, fexportedList, depList) <- fmap unzip3 . forM stgbinFileNames $ \fname -> do
+    (_, u, m, stub, hasForeignExport, deps) <- readDumpInfo fname
+    let fexport = if hasForeignExport then Just (u, m) else Nothing
+    pure (stub, fexport, ((u, m), [(du, dm) | (du, dl) <- deps, dm <- dl]))
   let fnameMap  = Map.fromList $ zip (map fst depList) stgbinFileNames
       mnameMap  = Map.fromList $ zip stgbinFileNames (map fst depList)
       depMap    = Map.fromList depList
@@ -74,12 +80,11 @@ collectImportedModules stgbinFileNames unitId mod = do
         | otherwise = Set.insert n s -- error $ printf "missing module: %s" . show $ getModuleName n
 
       keyMain = (UnitId $ BS8.pack unitId, ModuleName $ BS8.pack mod)
-      prunedDeps = catMaybes [Map.lookup m fnameMap | m <- Set.toList $ foldl calcDep mempty $ keyMain : rtsDeps]
+      prunedDeps = catMaybes [Map.lookup m fnameMap | m <- Set.toList $ foldl calcDep mempty $ keyMain : rtsDeps ++ catMaybes fexportedList]
       rtsDeps = [(UnitId $ BS8.pack u, ModuleName $ BS8.pack m) | (u, m, _) <- catMaybes $ map (decodePackageQualifiedName . packName) GHCSymbols.liveSymbols]
 
   putStrLn $ "dependencies:\n" ++ unlines ["  " ++ (show $! mnameMap Map.! fname) | fname <- prunedDeps]
-
-  pure prunedDeps
+  pure (stubs, prunedDeps)
 
 hashSize :: Int
 hashSize = 64
@@ -144,16 +149,34 @@ addMain prg = prg { pDefinitions = mainFun : pDefinitions prg} where
     , ("main_::Main.main_v01", SingleValue LiftedRep, App (packName "main_:Main.main") ["main_::Main.main_v00"])
     ] (Var "main_::Main.main_v01")
 
+writeStubs :: String -> [ForeignStubs] -> IO ()
+writeStubs fname stubs = do
+  let allStub = mconcat [(h <> "\n", c <> "\n") | ForeignStubs h c <- stubs] :: (BS8.ByteString, BS8.ByteString)
+  BS.writeFile (fname ++ ".stubsbin") . BSL.toStrict $ encode allStub
+
+parseSection :: [String] -> String -> [String]
+parseSection content n = map (read . tail) . takeWhile (isPrefixOf "-") . tail . dropWhile (not . isPrefixOf n) $ content
+
+getStgbins :: FilePath -> IO [FilePath]
+getStgbins fname = do
+  content <- lines <$> readFile fname
+  let paths   = parseSection content "pkg_lib_paths:"
+      [root]  = parseSection content "root:"
+      o_files = parseSection content "o_files:"
+  stgbins <- mapM (find always (extension ==? ".o_stgbin")) paths
+  pure $ [root </> (o ++ "_stgbin") | o <- o_files ] ++ concat stgbins
+
 cg_main :: Opts -> IO ()
 cg_main opts = do
-  let inputLen = length $ inputs opts
+  inputStgBins <- getStgbins $ input opts
+  let inputLen = length inputStgBins
   printf "input length: %d\n" inputLen
   r <- getResourceLimit ResourceOpenFiles
   print r
   let ResourceLimits (ResourceLimit minNum) (ResourceLimit maxNum) = r
   setResourceLimit ResourceOpenFiles $ ResourceLimits (ResourceLimit $ max minNum $ min (fromIntegral inputLen + minNum) maxNum) (ResourceLimit maxNum)
 
-  prunedDeps <- collectImportedModules (inputs opts) "main" "Main"
+  (stubs, prunedDeps) <- collectImportedModules inputStgBins "main" "Main"
   printf "pruned length: %d\n" $ length prunedDeps
 
   -- compile pruned program
@@ -165,9 +188,9 @@ cg_main opts = do
   let output_fn         = output opts
   writeFile (output_fn ++ ".lambda") . showWidth 800 . plain $ pretty wholeProgram
   lintLambda wholeProgram
-  printf "all: %d pruned: %d\n" (length $ inputs opts) (length prunedDeps)
-  let Program extsStripped  cgrouspStripped _ sdataStripped defsStripped  = wholeProgram
-      Program extsBloat     cgroupsBloat    _ sdataBloat    defsBloat     = wholeProgramBloat
+  printf "all: %d pruned: %d\n" (length inputStgBins) (length prunedDeps)
+  let Program extsStripped  cgrouspStripped _ _ sdataStripped defsStripped  = wholeProgram
+      Program extsBloat     cgroupsBloat    _ _ sdataBloat    defsBloat     = wholeProgramBloat
       conDefCount           = length [() | Def _ _ (Con{}) <- defsStripped]
   printf "bloat    lambda def count: %d\n" (length defsBloat)
   printf "stripped lambda def count: %d\n" (length defsStripped)
@@ -177,10 +200,11 @@ cg_main opts = do
   printf "def size histogram (size: count, sample def name)\n%s" $ unlines [printf "  % 7d: % 7d     %s" size count name | (size, (count, name)) <- Map.toList histogram]
 
   let pset = Set.fromList prunedDeps
-      aset = Set.fromList $ inputs opts
+      aset = Set.fromList inputStgBins
   --printf "dead modules:\n%s" (unlines $ Set.toList $ aset Set.\\ pset)
 
   BS.writeFile (output_fn ++ ".lambdabin") . BSL.toStrict $ encode wholeProgram
+  writeStubs output_fn stubs
   {-
   let lambdaGrin = codegenGrin wholeProgram
   writeFile (output_fn ++ ".grin") $ show $ plain $ pretty lambdaGrin
@@ -190,7 +214,7 @@ main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
   opts <- getOpts
-  if (null (inputs opts))
+  if (null (input opts))
      then showUsage
      else cg_main opts
 {-
