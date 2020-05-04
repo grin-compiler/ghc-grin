0d048c1d601a472c670ca0c53775071bdfbe8b05
Csaba Hruska <csaba.hruska@gmail.com>
2020-05-04 09:42:28 +0200

    add c files for the simplest sample project

--- ghc-grin-benchmark/app/Main.hs
+++ ghc-grin-benchmark/app/Main.hs
@@ -1,4 +1,15 @@
-import Hello
+{-# LANGUAGE ForeignFunctionInterface #-}
+--import Hello
+import Data.Word
 
-main = putStrLn f
+foreign import ccall "print_int64" print_int64 :: Word64 -> IO ()
 
+{-# NOINLINE test #-}
+test :: Int -> IO ()
+test x = do
+  print_int64 $ fromIntegral x
+
+main = do
+  putStrLn "hello!" --f
+  test 1
+  test 2
---
+++ ghc-grin-benchmark/app/main_c_lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void print_int64(int64_t i) {
+  printf("int64_t: %ld\n", i);
+}
